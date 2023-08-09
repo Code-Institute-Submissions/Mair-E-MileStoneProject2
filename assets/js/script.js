fb437328494b4136bbf89daa824a93e7c3eb58f0
Mair Edmunds <xmairx@hotmail.co.uk>
2023-08-09 16:43:26 +0000

    Add structure for js game-area layout and randomising cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,86 @@
-console.log("TESTING");
+const board = document.getElementById("board");
+
+/** ARRAY FOR GAME-AREA LAYOUT */
+let pairCards = () => [{
+        name: "xx",
+        img: "assets/images/xx.png"
+    },
+    {
+        name: "xx",
+        img: "assets/images/xx.png"
+    },
+    {
+        name: "xx",
+        img: "assets/images/xx.png"
+    },
+    {
+        name: "xx",
+        img: "assets/images/xx.png"
+    },
+    {
+        name: "xx",
+        img: "assets/images/xx.png"
+    },
+    {
+        name: "xx",
+        img: "assets/images/xx.png"
+    },
+    {
+        name: "xx",
+        img: "assets/images/xx.png"
+    },
+    {
+        name: "xx",
+        img: "assets/images/xx.png"
+    },
+    {
+        name: "xx",
+        img: "assets/images/xx.png"
+    },
+    {
+        name: "xx",
+        img: "assets/images/xx.png"
+    },
+    {
+        name: "xx",
+        img: "assets/images/xx.png"
+    },
+    {
+        name: "xx",
+        img: "assets/images/xx.png"
+    },
+    {
+        name: "xx",
+        img: "assets/images/xx.png"
+    },
+    {
+        name: "xx",
+        img: "assets/images/xx.png"
+    },
+    {
+        name: "xx",
+        img: "assets/images/xx.png"
+    },
+    {
+        name: "xx",
+        img: "assets/images/xx.png"
+    },
+    {
+        name: "xx",
+        img: "assets/images/xx.png"
+    },
+    {
+        name: "xx",
+        img: "assets/images/xx.png"
+    }, 
+];
+
+/** RANDOMIZES CARDS ON BOARD */
+let randomize = () => {
+    let cardInfo = pairCards();
+    //randomize the array from pairCards variable: //
+    cardInfo.sort(() => Math.random() - 0.5);
+    return cardInfo;
+};
+
+randomize();
