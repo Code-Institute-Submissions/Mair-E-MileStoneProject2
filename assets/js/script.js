022fafa8335cecea9a2ba636ac2a91e1d494eef4
Mair Edmunds <xmairx@hotmail.co.uk>
2023-08-17 17:39:30 +0000

    figuring out how to tidy up grid

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,159 +1,174 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-/** -- ARRAY FOR CARD OPTIONS --  */
-const cardArray = [{
-        name: "australia",
-        img: "assets/images/oz.JPG"
-    },
-    {
-        name: "china",
-        img: "assets/images/china.JPG"
-    },
-    {
-        name: "egypt",
-        img: "assets/images/egypt.JPG"
-    },
-    {
-        name: "france",
-        img: "assets/images/france.JPG"
-    },
-    {
-        name: "india",
-        img: "assets/images/india.JPG"
-    },
-    {
-        name: "italy",
-        img: "assets/images/italy.JPG"
-    },
-    {
-        name: "usa",
-        img: "assets/images/usa.JPG"
-    },
-    {
-        name: "uk",
-        img: "assets/images/uk.JPG"
-    },
-    {
-        name: "uk",
-        img: "assets/images/bigben.JPG"
-    },
-    {
-        name: "india",
-        img: "assets/images/tajmahal.JPG"
-    },
-    {
-        name: "italy",
-        img: "assets/images/pisa.JPG"
-    },
-    {
-        name: "france",
-        img: "assets/images/eiffeltower.JPG"
-    },
-    {
-        name: "usa",
-        img: "assets/images/statue.JPG"
-    },
-    {
-        name: "australia",
-        img: "assets/images/operahouse.JPG"
-    },
-    {
-        name: "egypt",
-        img: "assets/images/pyramid.JPG"
-    },
-    {
-        name: "china",
-        img: "assets/images/wall.JPG"
-    },
-];
+    /** -- ARRAY FOR CARD OPTIONS --  */
+    const cardArray = [{
+            name: "australia",
+            img: "assets/images/oz.JPG"
+        },
+        {
+            name: "china",
+            img: "assets/images/china.JPG"
+        },
+        {
+            name: "egypt",
+            img: "assets/images/egypt.JPG"
+        },
+        {
+            name: "france",
+            img: "assets/images/france.JPG"
+        },
+        {
+            name: "india",
+            img: "assets/images/india.JPG"
+        },
+        {
+            name: "italy",
+            img: "assets/images/italy.JPG"
+        },
+        {
+            name: "usa",
+            img: "assets/images/usa.JPG"
+        },
+        {
+            name: "uk",
+            img: "assets/images/uk.JPG"
+        },
+        {
+            name: "uk",
+            img: "assets/images/bigben.JPG"
+        },
+        {
+            name: "india",
+            img: "assets/images/tajmahal.JPG"
+        },
+        {
+            name: "italy",
+            img: "assets/images/pisa.JPG"
+        },
+        {
+            name: "france",
+            img: "assets/images/eiffeltower.JPG"
+        },
+        {
+            name: "usa",
+            img: "assets/images/statue.JPG"
+        },
+        {
+            name: "australia",
+            img: "assets/images/operahouse.JPG"
+        },
+        {
+            name: "egypt",
+            img: "assets/images/pyramid.JPG"
+        },
+        {
+            name: "china",
+            img: "assets/images/wall.JPG"
+        },
+    ];
 
-const grid = document.querySelector(".grid");
-const resultDisplay = document.querySelector('#result')
-let cardsChosen =[]
-let cardsChosenId =[]
-let CardsWon = []
+    const grid = document.querySelector(".grid");
+    const resultDisplay = document.querySelector('#result')
+    let cardsChosen = []
+    let cardsChosenId = []
+    let CardsWon = []
 
-/** -- RANDOMISE CARD ARRAY -- */
+    /** -- RANDOMISE CARD ARRAY -- */
 
-cardArray.sort(() => 0.5 - Math.random())
+    cardArray.sort(() => 0.5 - Math.random())
 
-/** -- CREATE BOARD -- */
-function createBoard() {
-    for (let i=0; i < cardArray.length; i++) {
-        let card = document.createElement('img')
-        card.setAttribute('src', 'assets/images/world.JPG')
-        card.setAttribute('data-id', i)
-        card.addEventListener('click', flipCard)
-        grid.appendChild(card)
+    /** -- CREATE BOARD -- */
+    function createBoard() {
+        for (let i = 0; i < cardArray.length; i++) {
+            let card = document.createElement('img')
+            card.setAttribute('src', 'assets/images/world.JPG')
+            card.setAttribute('data-id', i)
+            card.addEventListener('click', flipCard)
+            grid.appendChild(card)
+        }
+        console.log(createBoard);
     }
-    console.log(createBoard);
-}
 
 
 
-/** CHECK MATCH */
+    /** CHECK MATCH */
 
-function checkForMatch() {
-    let cards = document.querySelectorAll('img')
-    const optionOneId = cardsChosenId[0]
-    const optionTwoId = cardsChosenId[1]
-    if ((cardsChosen[0]) === cardsChosen[1]) {
-        alert('You found a match')
-        cards[optionOneId].setAttribute('src', 'assets/images/white.JPG')
-        cards[optionTwoId].setAttribute('src', 'assets/images/white.JPG')
-        cardsWon.push(cardsChosen)
-    } else {
-        cards[optionOneId].setAttribute('src', 'assets/images/world.JPG')
-        cards[optionTwoId].setAttribute('src', 'assets/images/world.JPG')
-        alert('Sorry, try again')
+    function checkForMatch() {
+        let cards = document.querySelectorAll('img')
+        const optionOneId = cardsChosenId[0]
+        const optionTwoId = cardsChosenId[1]
+        if ((cardsChosen[0]) === cardsChosen[1]) {
+            alert('You found a match.')
+            cards[optionOneId].setAttribute('src', 'assets/images/white.JPG')
+            cards[optionTwoId].setAttribute('src', 'assets/images/white.JPG')
+            cardsWon.push(cardsChosen)
+        } else {
+            cards[optionOneId].setAttribute('src', 'assets/images/world.JPG')
+            cards[optionTwoId].setAttribute('src', 'assets/images/world.JPG')
+            alert('Sorry, try again.')
+        }
+        cardsChosen = []
+        cardsChosenId = []
+        resultDisplay.textContent = CardsWon.length
+        if (CardsWon.lengh === cardArray.length / 2) {
+            resultDisplay.textContent = 'Congratulations! You found all the matches!'
+        };
+        console.log(checkForMatch);
     }
-    cardsChosen = []
-    cardsChosenId = []
-    resultDisplay.textContent = CardsWon.length
-    if (CardsWon.lengh === cardArray.length/2) {
-        resultDisplay.textContent = 'Congratulations! You found all the matches!'
-    };
-}
 
 
-/** -- FLIP CARD -- */
+    /** -- FLIP CARD -- */
 
-function flipCard() {
-    let cardId = this.getAttribute('data-id')
-    cardsChosen.push(cardArray[cardId].name)
-    cardsChosenId.push(cardId)
-    this.setAttribute('src', cardArray[cardId].img)
-    if (cardsChosen.length === 2) {
-        setTimeout(checkForMatch, 500)
+    function flipCard() {
+        let cardId = this.getAttribute('data-id')
+        cardsChosen.push(cardArray[cardId].name)
+        cardsChosenId.push(cardId)
+        this.setAttribute('src', cardArray[cardId].img)
+        if (cardsChosen.length === 2) {
+            setTimeout(checkForMatch, 500)
+        };
+        console.log(flipCard);
     }
-}
 
 
-/** -- NEW GAME -- */
+    /** -- NEW GAME -- */
 
-/** -- COUNT NUMBER OF MOVES AND TOTAL SCORE -- */
+    const resetButton = document.querySelector(".reset");
 
-/** -- HOW TO PLAY, pop-up OPEN and CLOSE instructions -- */
+    resetButton.addEventListener("click", () => {
+        moveContainer.innerHTML = 0;
+        moves = 0;
+        board.innerHTML = '';
+        createBoard();
+    });
 
-const rulesButton = document.getElementById("rules");
-let rules = document.getElementsByClassName("rules-card");
 
-console.log(rulesButton);
 
-rulesButton.addEventListener('click', showRules);
+    /** -- COUNT NUMBER OF MOVES AND TOTAL SCORE -- */
 
-function showRules() {
-    rules[0].style.display = "block";
-}
+    /** -- HOW TO PLAY, pop-up OPEN and CLOSE instructions -- */
 
-const playButton = document.querySelector(".play-button");
-rules = document.getElementsByClassName("rules-card");
+    const rulesButton = document.getElementById("rules");
+    let rules = document.getElementsByClassName("rules-card");
 
-playButton.addEventListener('click', hideRules);
+    console.log(rulesButton);
 
-function hideRules() {
-    rules[0].style.display = "none";
-}
+    rulesButton.addEventListener('click', showRules);
 
-createBoard()
+    function showRules() {
+        rules[0].style.display = "block";
+        console.log(showRules);
+    }
+
+    const playButton = document.querySelector(".play-button");
+    rules = document.getElementsByClassName("rules-card");
+
+    playButton.addEventListener('click', hideRules);
+
+    function hideRules() {
+        rules[0].style.display = "none";
+        console.log(hideRules);
+    }
+
+    createBoard()
 })
