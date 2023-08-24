369f59fce23246ac88f9abdd9d98b096805ab77e
Mair Edmunds <xmairx@hotmail.co.uk>
2023-08-24 09:40:57 +0000

    Add try, catch to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,212 +1,228 @@
 document.addEventListener('DOMContentLoaded', () => {
+    try {
 
-    /** -- ARRAY FOR CARD OPTIONS --  */
-    const cardArray = [{
-            name: "australia",
-            img: "assets/images/oz.jpg"
-        },
-        {
-            name: "china",
-            img: "assets/images/china.jpg"
-        },
-        {
-            name: "egypt",
-            img: "assets/images/egypt.jpg"
-        },
-        {
-            name: "france",
-            img: "assets/images/france.jpg"
-        },
-        {
-            name: "india",
-            img: "assets/images/india.jpg"
-        },
-        {
-            name: "italy",
-            img: "assets/images/italy.jpg"
-        },
-        {
-            name: "usa",
-            img: "assets/images/usa.jpg"
-        },
-        {
-            name: "uk",
-            img: "assets/images/uk.jpg"
-        },
-        {
-            name: "uk",
-            img: "assets/images/bigben.jpg"
-        },
-        {
-            name: "india",
-            img: "assets/images/tajmahal.jpg"
-        },
-        {
-            name: "italy",
-            img: "assets/images/pisa.jpg"
-        },
-        {
-            name: "france",
-            img: "assets/images/eiffeltower.jpg"
-        },
-        {
-            name: "usa",
-            img: "assets/images/statue.jpg"
-        },
-        {
-            name: "australia",
-            img: "assets/images/operahouse.jpg"
-        },
-        {
-            name: "egypt",
-            img: "assets/images/pyramid.jpg"
-        },
-        {
-            name: "china",
-            img: "assets/images/wall.jpg"
-        },
-    ];
+        /** -- ARRAY FOR CARD OPTIONS --  */
+        const cardArray = [{
+                name: "australia",
+                img: "assets/images/oz.jpg"
+            },
+            {
+                name: "china",
+                img: "assets/images/china.jpg"
+            },
+            {
+                name: "egypt",
+                img: "assets/images/egypt.jpg"
+            },
+            {
+                name: "france",
+                img: "assets/images/france.jpg"
+            },
+            {
+                name: "india",
+                img: "assets/images/india.jpg"
+            },
+            {
+                name: "italy",
+                img: "assets/images/italy.jpg"
+            },
+            {
+                name: "usa",
+                img: "assets/images/usa.jpg"
+            },
+            {
+                name: "uk",
+                img: "assets/images/uk.jpg"
+            },
+            {
+                name: "uk",
+                img: "assets/images/bigben.jpg"
+            },
+            {
+                name: "india",
+                img: "assets/images/tajmahal.jpg"
+            },
+            {
+                name: "italy",
+                img: "assets/images/pisa.jpg"
+            },
+            {
+                name: "france",
+                img: "assets/images/eiffeltower.jpg"
+            },
+            {
+                name: "usa",
+                img: "assets/images/statue.jpg"
+            },
+            {
+                name: "australia",
+                img: "assets/images/operahouse.jpg"
+            },
+            {
+                name: "egypt",
+                img: "assets/images/pyramid.jpg"
+            },
+            {
+                name: "china",
+                img: "assets/images/wall.jpg"
+            },
+        ];
 
-    const grid = document.querySelector(".grid");
-    const resultDisplay = document.querySelector('#result');
-    let cardsChosen = [];
-    let cardsChosenId = [];
-    let cardsWon = [];
-    let matchCount = 0;
+        const grid = document.querySelector(".grid");
+        const resultDisplay = document.querySelector('#result');
+        let cardsChosen = [];
+        let cardsChosenId = [];
+        let cardsWon = [];
+        let matchCount = 0;
 
-    /** -- RANDOMISE CARD ARRAY -- */
+        /** -- RANDOMISE CARD ARRAY -- */
 
-    cardArray.sort(() => 0.5 - Math.random());
-
-    /** -- CREATE BOARD -- */
-
-    function createBoard() {
-        for (let i = 0; i < cardArray.length; i++) {
-            let card = document.createElement('img');
-            card.setAttribute('src', 'assets/images/world.jpg');
-            card.setAttribute('class', 'images');
-            card.setAttribute('alt', 'heart shaped globe')
-            card.setAttribute('data-id', i);
-            card.addEventListener('click', flipCard);
-            grid.appendChild(card);
-        }
-        console.log("Creating board...");
-    }
-
-    /** CHECK MATCH AND RESET CARDS IF DO NOT MATCH -- */
-
-    function checkForMatch() {
-        let cards = document.querySelectorAll('.images');
-        console.log(cards);
-        const optionOneId = cardsChosenId[0];
-        const optionTwoId = cardsChosenId[1];
-
-        //Increment the match count
-        matchCount++;
-
-        if ((cardsChosen[0]) === cardsChosen[1]) {
-            alert('You found a match.');
-
-            cards[optionOneId].setAttribute('src', 'assets/images/tick.jpg');
-            cards[optionTwoId].setAttribute('src', 'assets/images/tick.jpg');
-            cardsWon.push(cardsChosen);
-        } else {
-            cards[optionOneId].setAttribute('src', 'assets/images/world.jpg');
-            cards[optionTwoId].setAttribute('src', 'assets/images/world.jpg');
-        }
-
-
-        /** -- ALL CARDS MATCHED AND GAME COMPLETE -- */
-
-        cardsChosen = [];
-        cardsChosenId = [];
-        resultDisplay.textContent = cardsWon.length;
-
-        if (cardsWon.length === cardArray.length / 2) {
-            resultDisplay.textContent = 'Congratulations! You found all the matches!';
-        }
-        console.log(checkForMatch);
-    }
-
-    /** -- FLIP CARD (AND STOP SAME CARD BEING CLICKED TWICE)-- */
-
-    let isFlipping = false;
-
-    function flipCard() {
-        if (isFlipping) {
-            return; // Ignore clicks while a card is being flipped
-        }
-
-        let cardId = this.getAttribute('data-id');
-
-        // Check if the clicked card has already been chosen
-        if (cardsChosenId.includes(cardId)) {
-            return; // Ignore clicks on already chosen cards
-        }
-        // Mark that a card is being flipped
-        isFlipping = true;
-
-        cardsChosen.push(cardArray[cardId].name);
-        cardsChosenId.push(cardId);
-        this.setAttribute('src', cardArray[cardId].img);
-        if (cardsChosen.length === 2) {
-            setTimeout(() => {
-                checkForMatch();
-                isFlipping = false; // Allow flipping again after checking for match
-            }, 750);
-        } else {
-            isFlipping = false; // Allow flipping again if only one card is chosen
-        }
-        console.log(flipCard);
-
-    }
-
-    /** -- RESET MATCHES COUNTER AND CLEAR THE BOARD -- */
-
-    const resetButton = document.querySelector(".reset");
-    const matchContainer = document.querySelector('.move-count');
-
-    resetButton.addEventListener("click", () => {
-        matchCount = 0;
-        matchContainer.textContent = matchCount;
-
-        // Reset matches count and update the display
-        cardsWon = [];
-        resultDisplay.textContent = cardsWon.length;
-
-
-        grid.innerHTML = '';
-        createBoard();
-        console.log("Button clicked!");
         cardArray.sort(() => 0.5 - Math.random());
-    });
 
-    /** -- OPEN HOW TO PLAY, pop-up instructions -- */
+        /** -- CREATE BOARD -- */
 
-    const rulesButton = document.getElementById("rules");
-    let rules = document.getElementsByClassName("rules-card");
+        function createBoard() {
+            try {
+                for (let i = 0; i < cardArray.length; i++) {
+                    let card = document.createElement('img');
+                    card.setAttribute('src', 'assets/images/world.jpg');
+                    card.setAttribute('class', 'images');
+                    card.setAttribute('alt', 'heart shaped globe')
+                    card.setAttribute('data-id', i);
+                    card.addEventListener('click', flipCard);
+                    grid.appendChild(card);
+                }
+                console.log("Creating board...");
+            } catch (error) {
+                console.error("An error occurred in createBoard:", error);
+            }
+        }
 
-    console.log(rulesButton);
+        /** CHECK MATCH AND RESET CARDS IF DO NOT MATCH -- */
 
-    rulesButton.addEventListener('click', showRules);
+        function checkForMatch() {
+            let cards = document.querySelectorAll('.images');
+            console.log(cards);
+            const optionOneId = cardsChosenId[0];
+            const optionTwoId = cardsChosenId[1];
 
-    function showRules() {
-        rules[0].style.display = "block";
+            //Increment the match count
+            matchCount++;
+
+            if ((cardsChosen[0]) === cardsChosen[1]) {
+                alert('You found a match.');
+
+                cards[optionOneId].setAttribute('src', 'assets/images/tick.jpg');
+                cards[optionTwoId].setAttribute('src', 'assets/images/tick.jpg');
+                cardsWon.push(cardsChosen);
+            } else {
+                cards[optionOneId].setAttribute('src', 'assets/images/world.jpg');
+                cards[optionTwoId].setAttribute('src', 'assets/images/world.jpg');
+            }
+
+
+            /** -- ALL CARDS MATCHED AND GAME COMPLETE -- */
+
+            cardsChosen = [];
+            cardsChosenId = [];
+            resultDisplay.textContent = cardsWon.length;
+
+            if (cardsWon.length === cardArray.length / 2) {
+                resultDisplay.textContent = 'Congratulations! You found all the matches!';
+            }
+            console.log(checkForMatch);
+        }
+
+        /** -- FLIP CARD (AND STOP SAME CARD BEING CLICKED TWICE)-- */
+
+        let isFlipping = false;
+
+        function flipCard() {
+            if (isFlipping) {
+                return; // Ignore clicks while a card is being flipped
+            }
+
+            let cardId = this.getAttribute('data-id');
+
+            // Check if the clicked card has already been chosen
+            if (cardsChosenId.includes(cardId)) {
+                return; // Ignore clicks on already chosen cards
+            }
+            // Mark that a card is being flipped
+            isFlipping = true;
+
+            cardsChosen.push(cardArray[cardId].name);
+            cardsChosenId.push(cardId);
+            this.setAttribute('src', cardArray[cardId].img);
+            if (cardsChosen.length === 2) {
+                setTimeout(() => {
+                    checkForMatch();
+                    isFlipping = false; // Allow flipping again after checking for match
+                }, 750);
+            } else {
+                isFlipping = false; // Allow flipping again if only one card is chosen
+            }
+            console.log(flipCard);
+
+        }
+
+        /** -- RESET MATCHES COUNTER AND CLEAR THE BOARD -- */
+
+        const resetButton = document.querySelector(".reset");
+        const matchContainer = document.querySelector('.move-count');
+
+        resetButton.addEventListener("click", () => {
+            try {
+                matchCount = 0;
+                matchContainer.textContent = matchCount;
+
+                // Reset matches count and update the display
+                cardsWon = [];
+                resultDisplay.textContent = cardsWon.length;
+
+
+                grid.innerHTML = '';
+                createBoard();
+                console.log("Button clicked!");
+                cardArray.sort(() => 0.5 - Math.random());
+            } catch (error) {
+                console.error("An error occurred in resetButton click event:", error);
+            }
+        });
+
+        /** -- OPEN HOW TO PLAY, pop-up instructions -- */
+
+        const rulesButton = document.getElementById("rules");
+        let rules = document.getElementsByClassName("rules-card");
+
+        console.log(rulesButton);
+
+        rulesButton.addEventListener('click', showRules);
+
+        function showRules() {
+            rules[0].style.display = "block";
+        }
+        console.log(showRules);
+
+        /** -- CLOSE HOW TO PLAY, pop-up instructions -- */
+
+        const playButton = document.querySelector(".play-button");
+        rules = document.getElementsByClassName("rules-card");
+
+        playButton.addEventListener('click', hideRules);
+
+        function hideRules() {
+            try {
+                rules[0].style.display = "none";
+            } catch (error) {
+                console.error("An error occurred in playButton click event:", error);
+            }
+        }
+        console.log(hideRules);
+
+        createBoard();
+
+    } catch (error) {
+        console.error("An error occurred:", error);
     }
-    console.log(showRules);
-
-    /** -- CLOSE HOW TO PLAY, pop-up instructions -- */
-
-    const playButton = document.querySelector(".play-button");
-    rules = document.getElementsByClassName("rules-card");
-
-    playButton.addEventListener('click', hideRules);
-
-    function hideRules() {
-        rules[0].style.display = "none";
-    }
-    console.log(hideRules);
-
-    createBoard();
-
 });
